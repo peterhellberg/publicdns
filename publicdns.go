8f144a35879980d31ed8b108fac56cd4a9bc9c56
Peter Hellberg <peter@c7.se>
2016-10-03 11:29:43 +0200

    Added comments

--- publicdns.go
+++ publicdns.go
@@ -1,3 +1,40 @@
+/*
+
+Package publicdns is a client for Google Public DNS
+
+Installation
+
+Just go get the package:
+
+go get -u github.com/peterhellberg/publicdns
+
+Usage
+
+    package main
+
+    import (
+    	"encoding/json"
+    	"fmt"
+    	"os"
+
+    	"github.com/peterhellberg/publicdns"
+    )
+
+    func main() {
+    	res, err := publicdns.Resolve("example.com", publicdns.ANY)
+    	if err != nil {
+    		fmt.Printf("Error: %v\n", err)
+    		os.Exit(1)
+    	}
+
+    	enc := json.NewEncoder(os.Stdout)
+
+    	enc.SetIndent("", "  ")
+
+    	enc.Encode(res)
+    }
+
+*/
 package publicdns
 
 import (
@@ -8,17 +45,30 @@ import (
 	"net/http"
 )
 
+// RRType is the resource record type
 type RRType string
 
 const (
-	A     RRType = "A"
-	AAAA  RRType = "AAAA"
+	// A is the address record
+	A RRType = "A"
+
+	// AAAA is the IPv6 address record
+	AAAA RRType = "AAAA"
+
+	// CNAME is the canonical name record
 	CNAME RRType = "CNAME"
-	MX    RRType = "MX"
-	PTR   RRType = "PTR"
-	ANY   RRType = "ANY"
+
+	// MX is the mail exchange record
+	MX RRType = "MX"
+
+	// PTR is the pointer record
+	PTR RRType = "PTR"
+
+	// ANY is a wildcard record in Google Public DNS
+	ANY RRType = "ANY"
 )
 
+// Response returned from Google Public DNS
 type Response struct {
 	Status    int        `json:"Status"`
 	TC        bool       `json:"TC"`
@@ -31,11 +81,13 @@ type Response struct {
 	Comment   string     `json:"Comment"`
 }
 
+// Question is returned in the response
 type Question struct {
 	Name string `json:"name"`
 	Type int    `json:"type"`
 }
 
+// Answer is returned in the response
 type Answer struct {
 	Name string `json:"name"`
 	Type int    `json:"type"`
@@ -43,12 +95,15 @@ type Answer struct {
 	Data string `json:"data"`
 }
 
+// DefaultClient is the default client
 var DefaultClient = &Client{http.DefaultClient}
 
+// Client is the Google Public DNS client
 type Client struct {
 	httpClient *http.Client
 }
 
+// Resolve resolves the name and RR type
 func (c *Client) Resolve(name string, rrType RRType) (*Response, error) {
 	resp, err := c.httpClient.Get(rawurl(name, rrType))
 	if err != nil {
@@ -68,6 +123,7 @@ func (c *Client) Resolve(name string, rrType RRType) (*Response, error) {
 	return r, nil
 }
 
+// Resolve resolves the name and RR type
 func Resolve(name string, rrType RRType) (*Response, error) {
 	return DefaultClient.Resolve(name, rrType)
 }
